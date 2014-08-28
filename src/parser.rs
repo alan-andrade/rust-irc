9bfc3c19b0cb550cac36a3c3cb50ba00ea5852b3
Alan Andrade <alan.andradec@gmail.com>
2014-08-28 00:10:19 -0700

    Improve code by using a buffer in the parser

--- src/parser.rs
+++ src/parser.rs
@@ -5,14 +5,15 @@
 pub struct Parser<I> {
     reader: Box<I>,
     state: State,
-    ch: Option<char>
+    ch: Option<char>,
+    buffer: String
 }
 
 pub struct Message {
-    pub prefix: String,
+    pub prefix:  String,
     pub command: String,
-    pub params: String,
-    pub body: String
+    pub params:  String,
+    pub body:    String,
 }
 
 #[deriving(PartialEq, Show, Clone)]
@@ -24,14 +25,13 @@ pub enum State {
     Body
 }
 
-pub type Token = (State, String);
-
 impl<I: Buffer> Parser<I> {
     pub fn new(rdr: Box<I>) -> Parser<I> {
         Parser {
             reader: rdr,
             ch: Some('\x00'),
-            state: Start
+            state: Start,
+            buffer: String::new()
         }
     }
 
@@ -51,7 +51,7 @@ impl<I: Buffer> Parser<I> {
         self.ch.is_none()
     }
 
-    fn parse_prefix(&mut self) -> Token {
+    fn parse_prefix(&mut self) {
         let mut prefix = String::new();
 
         loop {
@@ -60,10 +60,11 @@ impl<I: Buffer> Parser<I> {
             prefix.push_char(self.ch_or_null());
         }
 
-        (Prefix, prefix)
+        self.state = Prefix;
+        self.buffer.push_str(prefix.as_slice());
     }
 
-    fn parse_command(&mut self, numeric: bool) -> Token {
+    fn parse_command(&mut self, numeric: bool) {
         let mut command = String::new();
 
         if numeric {
@@ -79,10 +80,11 @@ impl<I: Buffer> Parser<I> {
             }
         }
 
-        (Command, command)
+        self.state = Command;
+        self.buffer.push_str(command.as_slice());
     }
 
-    fn parse_params(&mut self) -> Token {
+    fn parse_params(&mut self) {
         let mut params = String::new();
 
         loop {
@@ -91,10 +93,11 @@ impl<I: Buffer> Parser<I> {
             self.bump();
         }
 
-        (Params, params)
+        self.state = Params;
+        self.buffer.push_str(params.as_slice());
     }
 
-    fn parse_message(&mut self) -> Token {
+    fn parse_message(&mut self) {
         let mut msg = String::new();
 
         loop {
@@ -104,7 +107,8 @@ impl<I: Buffer> Parser<I> {
             msg.push_char(self.ch_or_null());
         }
 
-        (Body, msg)
+        self.state = Body;
+        self.buffer.push_str(msg.as_slice());
     }
 
     pub fn messages<'a>(&'a mut self) -> MessageIterator<'a, I> {
@@ -112,17 +116,18 @@ impl<I: Buffer> Parser<I> {
     }
 }
 
-impl<I: Buffer> Iterator<Token> for Parser<I> {
-    fn next(&mut self) -> Option<Token> {
+impl<I: Buffer> Iterator<String> for Parser<I> {
+    fn next(&mut self) -> Option<String> {
         self.bump();
+        self.buffer.clear();
 
-        let token = match self.ch_or_null() {
+        match self.ch_or_null() {
             ':' => {
                 match self.state {
-                    Start => { self.parse_prefix() }
-                    Params => { self.parse_message() }
-                    Command => { self.parse_message() }
-                    _ => (Start, String::new())
+                    Start   => { self.parse_prefix(); }
+                    Params  => { self.parse_message(); }
+                    Command => { self.parse_message(); }
+                    _ => { }
                 }
             }
 
@@ -130,18 +135,17 @@ impl<I: Buffer> Iterator<Token> for Parser<I> {
             c @ 'a'..'z' |
             c @ '0'..'9' => {
                 match self.state {
-                    Prefix | Start | Body=> { self.parse_command(c.is_digit()) }
-                    _ => { self.parse_params() }
+                    Prefix  |
+                    Start   |
+                    Body    => { self.parse_command(c.is_digit()); }
+                    _       => { self.parse_params(); }
                 }
             }
 
-            _ => (Start, String::new())
+            _ => { }
+        }
 
-        };
-
-        self.state = token.ref0().clone(); // TODO: Why do I have to clone?
-
-        Some(token)
+        Some(self.buffer.clone())
     }
 }
 
@@ -158,19 +162,23 @@ impl<'a, I: Buffer> Iterator<Message> for MessageIterator<'a, I> {
             body: String::new()
         };
 
-        let mut token = self.parser.next();
+        let mut has_text = self.parser.next();
 
-        while token.is_some() {
-            let (c, s) = token.unwrap();
-            match c {
-                Prefix => { msg.prefix = s }
-                Command => { msg.command = s }
-                Params => { msg.params = s }
-                Body => { msg.body = s; break; }
-                _ => { break; }
+        while has_text.is_some() {
+            let text_str = has_text.unwrap();
+            let text = text_str.as_slice();
+
+            match self.parser.state {
+                Prefix  =>  { msg.prefix.push_str(text) }
+                Command =>  { msg.command.push_str(text) }
+                Params  =>  { msg.params.push_str(text) }
+                Body    =>  { msg.body.push_str(text);
+                              break;
+                            }
+                _       =>  { break; }
             }
 
-            token = self.parser.next();
+            has_text = self.parser.next();
         }
 
         Some(msg)
@@ -186,8 +194,8 @@ mod test {
     #[test]
     fn test_commands() {
         macro_rules! test_token(
-            ($next: expr, $state: ident, $string: expr) => (
-                assert_eq!($next, Some(($state, $string.to_string())));
+            ($next: expr, $string: expr) => (
+                assert_eq!($next, Some($string.to_string()));
             )
         )
 
@@ -198,12 +206,12 @@ mod test {
         let mut buf = box BufReader::new(example.as_bytes());
         let mut parser = Parser::new(buf);
 
-        test_token!(parser.next(), Prefix, "Angel!wings@irc.org");
-        test_token!(parser.next(), Command, "PRIVMSG");
-        test_token!(parser.next(), Params, "Wiz");
-        test_token!(parser.next(), Body, "Are you receiving this message ?");
-        test_token!(parser.next(), Command, "PING");
-        test_token!(parser.next(), Body, "irc.funet.fi");
+        test_token!(parser.next(), "Angel!wings@irc.org");
+        test_token!(parser.next(), "PRIVMSG");
+        test_token!(parser.next(), "Wiz");
+        test_token!(parser.next(), "Are you receiving this message ?");
+        test_token!(parser.next(), "PING");
+        test_token!(parser.next(), "irc.funet.fi");
     }
 
     #[test]
